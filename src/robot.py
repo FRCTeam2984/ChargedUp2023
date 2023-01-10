9933e090e4cd38b46cae615cf0c73432f159294c
team2984 <team2984Git@gmail.com>
2023-01-09 17:20:58 -0800

    Template code for robot.py and folder structure

---
+++ src/robot.py
@@ -0,0 +1,19 @@
+import wpilib
+
+class MyRobot(wpilib.TimedRobot):
+
+   def robotInit(self):
+      self.timer = wpilib.Timer()
+
+   def autonomoutInit(self):
+      self.timer.reset()
+      self.timer.start()
+
+   def autonomousPeriodic(self):
+      pass
+
+   def teleopPeriodic(self):
+      pass
+
+if __name__ == "__main__":
+   wpilib.run(MyRobot)
