eb1c23e0a0defefa63b8857f8dd1117e7932d37e
SchoolComp2984 <team2984Git@gmail.com>
2023-02-07 17:06:22 -0800

    changes 2/7

--- src/commands/arm.py
+++ src/commands/arm.py
@@ -7,25 +7,34 @@ from rev import CANSparkMax
 from utils import math_functions
 
 class Arm:
-   def __init__(self, _arm_elevator_motor : CANSparkMax, _arm_chain_motor : CANSparkMax, _arm_end_servo : Servo):
+   def __init__(self, _arm_elevator_motor : CANSparkMax, _arm_base_motor : CANSparkMax, _arm_end_servo_1 : Servo, _arm_end_servo_2 : Servo):
       self.arm_elevator_motor = _arm_elevator_motor
-      self.arm_chain_motor = _arm_chain_motor
-      self.arm_end_servo = _arm_end_servo
+      self.arm_base_motor = _arm_base_motor
+
+      self.arm_end_servo_1 = _arm_end_servo_1
+      self.arm_end_servo_2 = _arm_end_servo_2
 
 
-   def setArmServoAngle(self, angle):
-      # clamp angle in future to make sure in range
-      self.arm_end_servo.setAngle(angle)
+   def set_servo_1_angle(self, angle):
+      clamped_angle = math_functions.clamp(angle, 0, 360)
+      self.arm_end_servo_1.setAngle(clamped_angle)
 
+   def set_servo_2_angle(self, angle):
+      clamped_angle = math_functions.clamped(angle, 0, 360)
+      self.arm_end_servo_2.setAngle(clamped_angle)
 
    def raise_elevator(self, speed):
-      speed = math_functions.clamp(speed, 1, -1)
-      self.arm_elevator_motor.set(speed)
+      clamped_speed = math_functions.clamp(speed, 1, -1)
+      self.arm_elevator_motor.set(clamped_speed)
 
    def lower_elevator(self, speed):
-      speed = math_functions.clamp(speed, 1, -1)
-      self.arm_elevator_motor.set(speed)
+      clamped_speed = math_functions.clamp(speed, 1, -1)
+      self.arm_elevator_motor.set(clamped_speed)
 
    def move_chain(self, speed):
-      speed = math_functions.clamp(-1, 1)
-      self.arm_chain_motor.set(speed)
+      clamped_speed = math_functions.clamp(speed, -1, 1)
+      self.arm_chain_motor.set(clamped_speed)
+
+
+   def position_home(self):
+      pass
