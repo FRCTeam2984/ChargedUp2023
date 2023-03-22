4ab85f027c4f4d795f80c35a76ff94bff9a8a76e
SchoolComp2984 <team2984Git@gmail.com>
2023-03-21 18:03:39 -0700

    nutsack

--- src/subsystems/camera_led.py
+++ src/subsystems/camera_led.py
@@ -2,18 +2,17 @@ from wpilib import PWM
 from utils import math_functions
 
 class Camera_LED:
-   def __init__(self, _left_camera_led : PWM, _right_camera_led : PWM):
+   def __init__(self, _left_camera_led : PWM):
       self.left_camera_led = _left_camera_led
-      self.right_camera_led = _right_camera_led
 
-   def set_left_led(self, brightness):
+   def set_led(self, brightness):
       clamped_brightness = math_functions.clamp(brightness, 0, 255)
       self.left_camera_led.setRaw(clamped_brightness)
 
-   def set_right_led(self, brightness):
+"""   def set_right_led(self, brightness):
       clamped_brightness = math_functions.clamp(brightness, 0, 255)
       self.right_camera_led.setRaw(clamped_brightness)
 
    def set_both_led(self, brightness):
       self.set_left_led(brightness)
-      self.set_right_led(brightness)
+      self.set_right_led(brightness)"""
